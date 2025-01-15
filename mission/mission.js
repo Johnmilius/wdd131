5567f19e0cd3effd84ffc9848a58f6bed5cf6543
johnmilius <mil24086@byui.edu>
2025-01-15 11:09:19 -0800

    prove 2

---
+++ mission/mission.js
@@ -0,0 +1,29 @@
+const themeSelector = document.querySelector('#theme-selector');// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+function changeTheme() {
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+    const selectedTheme = themeSelector.value;
+
+    if (themeSelector === 'dark'){
+        document.body.classList.add('dark');
+        const logo = document.querySelector('footer img');
+        logo.src = "byui-logo-black.png";
+    }
+    else {
+        document.body.classList.remove('dark');
+        const logo = document.querySelector('footer img');
+        logo.src = "byui-logo-blue.webp"; 
+    }
+
+
+// if the value is dark then:
+// add the dark class to the body
+// change the source of the logo img to point to the white logo.
+// otherwise
+// remove the dark class
+// make sure the logo src is the blue logo.
+}
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
