17b83ac9ccdf92487187c9f674de209071f5b3c3
johnmilius <mil24086@byui.edu>
2025-02-15 12:25:46 -0800

    without full form heighth

--- blog/blog.js
+++ blog/blog.js
@@ -4,7 +4,7 @@ const articles = [
 		title: 'Septimus Heap Book One: Magyk',
 		date: 'July 5, 2022',
 		description:
-			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you. <a href="#">Read More...</a>',
 		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
 		imgAlt: 'Book cover for Septimus Heap 1',
 		ages: '10-14',
@@ -23,5 +23,44 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
-]
+];
+
+
+const htmlTemplate = (articles) => {
+	return (
+	`<div class="filters">
+		<p>${articles.date}</p>
+		<p>${articles.ages}</p>
+		<p>${articles.genre}</p>
+		<p>${articles.stars}</p>
+  	</div>
+
+ 	 <div class="book-description-box">
+		<h2>${articles.title}</h2>
+		<img
+	  	src='${articles.imgSrc}'
+	  	alt='${articles.imgAlt}'
+		/>
+		<p>
+	  		${articles.description}
+		</p>
+  	</div>`
+);
+};
+
+const combinedHtmlTemplate = articles.map(htmlTemplate).join('');
+document.querySelector('#maincontent').innerHTML += combinedHtmlTemplate;
--- practice/array_practice.js
+++ practice/array_practice.js
@@ -1,11 +1,20 @@
 //  arrays.js
 const steps = ["one", "two", "three"];
-const listTemplate(step) {
-  return `<li>${step}<li>`//the html string made from step
-}
-const stepsHtml = // use map to convert the list from strings to HTML
-document.querySelector("#myList").innerHTML = // set the innerHTML
+const listTemplate = (step) => {
+  return `<li>${step}</li>`;//the html string made from step
+};
+const stepsHtml = steps.map(listTemplate).join("");// use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = stepsHtml;// set the innerHTML
 
-let new_array = arr.map(function callback( currentValue[, index[, array]]) {
-    // return element for new_array
-}[, thisArg])
+
+const grades = ["A", "B", "A"];
+function convertGradeToPoints(grade) {
+  let points = 0;
+  if (grade === "A") {
+    points = 4;
+  } else if (grade === "B") {
+    points = 3;
+  }
+  return points;
+}
+const gpaPoints = grades.map(convertGradeToPoints);
